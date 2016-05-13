a0857f55b6998101ebb856ab6b41e8ddf6881296
Ivanilson Lino <guerrito-bm@hotmail.com>
2016-05-12 21:56:44 -0300

    Add arquivo cadastra-veiculo

---
+++ cadastra-veiculo.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Parker
+ * Date: 12-05-2016
+ * Time: 21:34
+ */
+
+// Recebe todas as variaveis do formulario (inputs)
+
+$nomeUsuario = $_POST['user_name'];
+$userMail = $_POST['user_email'];
+$marca = $_POST['marca'];
+
+
+// Conexao com BD
+
+
+//Inserir motoristas no BD
+//Pesquisar: Como fazer cadastro com php e mysql.
+
+
+
+echo $marca;
