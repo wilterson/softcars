f6ea45d21be92f0c282b17a6d8ff09387b399f3b
Ivanilson Lino <guerrito-bm@hotmail.com>
2016-05-25 16:57:34 -0300

    Ultimas atualizações

--- cadastra-passageiro.php
+++ cadastra-passageiro.php
@@ -1,5 +1,19 @@
 <?php
+$host = "locallhost";
+$user = "root";
+$pass = "";
+$banco = "test";
+$link = mysql_connect($host, $user, $pass) or die (mysql_error());
+$db = mysql_select_db($banco, $link) or die (mysql_error());
 
+if(!$link)
+{
+    echo "erro ao conectar ao banco de dados!";exit();
+}
+
+?>
+
+<?php
 $nomeUsuario = $_POST['user_name'];
 $userMail = $_POST['user_email'];
 $userPassword = $_POST['user_password'];
@@ -11,6 +25,12 @@ $userRua = $_POST['user_rua'];
 $userNumero = $_POST['user_numero'];
 $userComplemento = $_POST['user_complemento'];
 
+$sql= mysql_query("INSERT INTO exemplo1 (user_name, user_email, user_password, user_age, user_cep, user_estado, user_cidade, user_rua, user_numero, user_complemento) 
+VALUES ('user_name' , 'user_email' , 'user_password' , 'user_age' , 'user_cep' , 'user_estado' , 'user_cidade' , 'user_rua' , 'user_numero' , 'user_complemento')");
+
+mysql_close($link);
+
+?>
 
 
 
